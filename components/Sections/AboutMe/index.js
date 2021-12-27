b593b1a35c3ebe6d403b44ced59c1a739c73820e
Liberontissauri <jp.liberon@gmail.com>
2021-12-27 20:10:09 +0000

    added about me section

---
+++ components/Sections/AboutMe/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+import Styles from "./index.module.css"
+
+function AboutMe() {
+    return (
+        <div className={Styles.container}>
+            <h2 className={Styles.title}>Who am I?</h2>
+            <p className={Styles.text}>Heyo, my name is João Liberato, and I’ve been 
+                learning software development for the last 4 years
+                 or so! I’m currently into web development, from frontend to 
+                 backend, but sometimes I also do modding for various games.
+            </p>
+        </div>
+    )
+}
+
+export default AboutMe
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Top from "../components/Sections/Top"
+import AboutMe from '../components/Sections/AboutMe'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         </svg>
       </div>
       <Top></Top>
+      <AboutMe></AboutMe>
     </div>
   )
 }
